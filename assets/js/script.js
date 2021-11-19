12f86059fae3893589bf700d0f11a333ce0c88ca
kimberlyamaya <kimberly.kimbell89@gmail.com>
2021-11-19 15:00:36 -0800

    finished up adding the weather cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,12 +13,17 @@ var excludeAPI = "minutely,hourly"
 var cityPlusDate = document.getElementById("city-plus-date")
 var currentWeatherContainer = document.getElementById("current-weather")
 var currentWeatherHeaderIconDiv = document.getElementById("headerIconDiv")
-var forecastWeaterContainer = document.getElementById("5dayWeather")
-var forecastHeader = document.getElementById("5dayForecastHeader")
+var forecastWeaterContainer = document.getElementById("fivedayWeather")
+var forecastHeader = document.getElementById("fivedayForecastHeader")
 var cardBody1 = document.getElementById("card-body-1")
+var cardBody2 = document.getElementById("card-body-2")
+var cardBody3 = document.getElementById("card-body-3")
+var cardBody4 = document.getElementById("card-body-4")
+var cardBody5 = document.getElementById("card-body-5")
 var historyWeatherContainer = document.getElementById("history-weather")
 var cityHistoryBtn = document.createElement("button")
 var todayDate = moment().format("l");
+var currentIcon1 = document.createElement("img")
 var currentTemp = document.createElement("p")
 var currentWind = document.createElement("p")
 var currentHumidity = document.createElement("p")
@@ -28,7 +33,27 @@ var forecastTemp1 = document.createElement("p")
 var forecastWind1 = document.createElement("p")
 var forecastHumidity1 = document.createElement("p")
 var forecasticon1 = document.createElement("img")
-var currentIcon1 = document.createElement("img")
+var forecastDate2 = document.createElement("h7")
+var forecastTemp2 = document.createElement("p")
+var forecastWind2 = document.createElement("p")
+var forecastHumidity2 = document.createElement("p")
+var forecasticon2 = document.createElement("img")
+var forecastDate3 = document.createElement("h7")
+var forecastTemp3 = document.createElement("p")
+var forecastWind3 = document.createElement("p")
+var forecastHumidity3 = document.createElement("p")
+var forecasticon3 = document.createElement("img")
+var forecastDate4 = document.createElement("h7")
+var forecastTemp4 = document.createElement("p")
+var forecastWind4 = document.createElement("p")
+var forecastHumidity4 = document.createElement("p")
+var forecasticon4 = document.createElement("img")
+var forecastDate5 = document.createElement("h7")
+var forecastTemp5 = document.createElement("p")
+var forecastWind5 = document.createElement("p")
+var forecastHumidity5 = document.createElement("p")
+var forecasticon5 = document.createElement("img")
+
 
 
 var inputHandler = function() {
@@ -200,7 +225,6 @@ var displayForecastWeatherData = function(weatherData) {
     forecastDate1.style.visibility="visible";
 
     // get icon1
-    console.log(weatherData.daily[1])
     forecasticon1.src = "https://openweathermap.org/img/wn/" + weatherData.daily[1].weather[0].icon + "@2x.png"
     forecasticon1.setAttribute("width","70")
     forecasticon1.setAttribute("height","70")
@@ -209,33 +233,156 @@ var displayForecastWeatherData = function(weatherData) {
     
     //get temp1
     forecastTemp1.textContent = "Temp: " + weatherData.daily[1].temp.day + " F"
-    forecastTemp1.classList.add("card-text")
     forecastTemp1.style.visibility="visible";
 
     //get wind1
     forecastWind1.textContent = "Wind: " + weatherData.daily[1].wind_speed + " MPH"
-    forecastWind1.classList.add("card-text")
     forecastWind1.style.visibility="visible";
 
     //get humidity1
     forecastHumidity1.textContent = "Humidity: " + weatherData.daily[1].humidity + " %"
-    forecastHumidity1.classList.add("card-text")
     forecastHumidity1.style.visibility="visible";
 
-    //apppend1
+
+    // ** CARD 2 ** //
+    //covert date2
+    unixTime2 = weatherData.daily[2].dt
+    var date2 = new Date(unixTime2 * 1000)
+    forecastDate2.textContent=date2.toLocaleDateString("en-US")
+    forecastDate2.classList.add("card-title")
+    forecastDate2.style.visibility="visible";
+
+    // get icon2
+    forecasticon2.src = "https://openweathermap.org/img/wn/" + weatherData.daily[2].weather[0].icon + "@2x.png"
+    forecasticon2.setAttribute("width","70")
+    forecasticon2.setAttribute("height","70")
+    forecasticon2.classList.add("forecast-icon")
+    forecasticon2.style.visibility="visible";
+    
+    //get temp2
+    forecastTemp2.textContent = "Temp: " + weatherData.daily[2].temp.day + " F"
+    forecastTemp2.style.visibility="visible";
+
+    //get wind2
+    forecastWind2.textContent = "Wind: " + weatherData.daily[2].wind_speed + " MPH"
+    forecastWind2.style.visibility="visible";
+
+    //get humidity2
+    forecastHumidity2.textContent = "Humidity: " + weatherData.daily[2].humidity + " %"
+    forecastHumidity2.style.visibility="visible";
+
+
+    // ** CARD 3 ** //
+    unixTime3 = weatherData.daily[3].dt
+    var date3 = new Date(unixTime3 * 1000)
+    forecastDate3.textContent=date3.toLocaleDateString("en-US")
+    forecastDate3.classList.add("card-title")
+    forecastDate3.style.visibility="visible";
+
+    // get icon3
+    forecasticon3.src = "https://openweathermap.org/img/wn/" + weatherData.daily[3].weather[0].icon + "@2x.png"
+    forecasticon3.setAttribute("width","70")
+    forecasticon3.setAttribute("height","70")
+    forecasticon3.classList.add("forecast-icon")
+    forecasticon3.style.visibility="visible";
+    
+    //get temp3
+    forecastTemp3.textContent = "Temp: " + weatherData.daily[3].temp.day + " F"
+    forecastTemp3.style.visibility="visible";
+
+    //get wind3
+    forecastWind3.textContent = "Wind: " + weatherData.daily[3].wind_speed + " MPH"
+    forecastWind3.style.visibility="visible";
+
+    //get humidity3
+    forecastHumidity3.textContent = "Humidity: " + weatherData.daily[3].humidity + " %"
+    forecastHumidity3.style.visibility="visible";
+
+
+    // ** CARD 4 ** //
+    unixTime4 = weatherData.daily[4].dt
+    var date4 = new Date(unixTime4 * 1000)
+    forecastDate4.textContent=date4.toLocaleDateString("en-US")
+    forecastDate4.classList.add("card-title")
+    forecastDate4.style.visibility="visible";
+
+    // get icon4
+    forecasticon4.src = "https://openweathermap.org/img/wn/" + weatherData.daily[4].weather[0].icon + "@2x.png"
+    forecasticon4.setAttribute("width","70")
+    forecasticon4.setAttribute("height","70")
+    forecasticon4.classList.add("forecast-icon")
+    forecasticon4.style.visibility="visible";
+    
+    //get temp4
+    forecastTemp4.textContent = "Temp: " + weatherData.daily[4].temp.day + " F"
+    forecastTemp4.style.visibility="visible";
+
+    //get wind4
+    forecastWind4.textContent = "Wind: " + weatherData.daily[4].wind_speed + " MPH"
+    forecastWind4.style.visibility="visible";
+
+    //get humidity4
+    forecastHumidity4.textContent = "Humidity: " + weatherData.daily[4].humidity + " %"
+    forecastHumidity4.style.visibility="visible";
+
+
+    // ** CARD 5 ** //
+    unixTime5 = weatherData.daily[5].dt
+    var date5 = new Date(unixTime5 * 1000)
+    forecastDate5.textContent=date5.toLocaleDateString("en-US")
+    forecastDate5.classList.add("card-title")
+    forecastDate5.style.visibility="visible";
+
+    // get icon5
+    forecasticon5.src = "https://openweathermap.org/img/wn/" + weatherData.daily[5].weather[0].icon + "@2x.png"
+    forecasticon5.setAttribute("width","70")
+    forecasticon5.setAttribute("height","70")
+    forecasticon5.classList.add("forecast-icon")
+    forecasticon5.style.visibility="visible";
+    
+    //get temp5
+    forecastTemp5.textContent = "Temp: " + weatherData.daily[5].temp.day + " F"
+    forecastTemp5.style.visibility="visible";
+
+    //get wind5
+    forecastWind5.textContent = "Wind: " + weatherData.daily[5].wind_speed + " MPH"
+    forecastWind5.style.visibility="visible";
+
+    //get humidity5
+    forecastHumidity5.textContent = "Humidity: " + weatherData.daily[5].humidity + " %"
+    forecastHumidity5.style.visibility="visible";
+
+
+    //apppend All
     cardBody1.appendChild(forecastDate1);
     cardBody1.appendChild(forecasticon1);
     cardBody1.appendChild(forecastTemp1);
     cardBody1.appendChild(forecastWind1);
     cardBody1.appendChild(forecastHumidity1);
 
-    // ** CARD 2 ** //
+    cardBody2.appendChild(forecastDate2);
+    cardBody2.appendChild(forecasticon2);
+    cardBody2.appendChild(forecastTemp2);
+    cardBody2.appendChild(forecastWind2);
+    cardBody2.appendChild(forecastHumidity2);
 
-    // ** CARD 3 ** //
+    cardBody3.appendChild(forecastDate3);
+    cardBody3.appendChild(forecasticon3);
+    cardBody3.appendChild(forecastTemp3);
+    cardBody3.appendChild(forecastWind3);
+    cardBody3.appendChild(forecastHumidity3);
 
-    // ** CARD 4 ** //
+    cardBody4.appendChild(forecastDate4);
+    cardBody4.appendChild(forecasticon4);
+    cardBody4.appendChild(forecastTemp4);
+    cardBody4.appendChild(forecastWind4);
+    cardBody4.appendChild(forecastHumidity4);
 
-    // ** CARD 5 ** //
+    cardBody5.appendChild(forecastDate5);
+    cardBody5.appendChild(forecasticon5);
+    cardBody5.appendChild(forecastTemp5);
+    cardBody5.appendChild(forecastWind5);
+    cardBody5.appendChild(forecastHumidity5);
 
     appendHistoryButtons();
 
